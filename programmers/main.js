2be7fff127608135d130ef6cce1a67d0dcfbe9df
JeonSuna <jsa020910@gmail.com>
2024-12-02 23:58:04 +0900

    추억 점수

--- programmers/main.js
+++ programmers/main.js
@@ -1,36 +1,34 @@
-//인덱스를 찾아야함
+const name = ['may', 'kein', 'kain', 'radi'];
+const yearning = [5, 10, 1, 3];
 
-function solution(s, skip, index) {
-  var answer = '';
-  const alp = 'abcdefghijklmnopqrstuvwxyz';
+const photo = [
+  ['may', 'kein', 'kain', 'radi'],
+  ['may', 'kein', 'brin', 'deny'],
+  ['kon', 'kain', 'may', 'coni'],
+];
 
-  const skipA = [...skip]; //건너뛸 문자열
-  //   console.log(skipA);
-  //   console.log(...alp);
-  let skipAlp = [...alp].filter((elem) => {
-    return !skipA.includes(elem);
-  });
-  console.log(skipAlp); //=> skip뺀 alp배열
+function solution(name, yearning, photo) {
+  const nameObj = new Map();
 
-  //   console.log([...s]);
-
-  for (const item of s) {
-    console.log(item);
-
-    console.log(skipAlp.indexOf(item));
-    let nowItem = skipAlp.indexOf(item); //skip 뺀 알파벳 배열에서의 인덱스값
-    if (nowItem === -1) continue; //=>없으면 건너뛰기
-    let newItem = (nowItem + index) % skipAlp.length;
-    console.log(newItem);
-    // 조건 => index만큼 뒤의 알파벳이z를 넘어갈 경우 다시 a로 돌아간다)
-    // while (newItem >= skipAlp.length) {
-    //   newItem = newItem - skipAlp.length;
-    // }
-    answer += skipAlp[newItem];
-    // console.log(nowItem); //인덱스
+  for (let i = 0; i < name.length; i++) {
+    nameObj.set(name[i], yearning[i]);
+    console.log(nameObj);
   }
+  var answer = [];
 
+  console.log(photo.length);
+  for (let i = 0; i < photo.length; i++) {
+    let sum = 0;
+    // console.log(photo[i]);
+    for (let k = 0; k < photo[i].length; k++) {
+      const person = photo[i][k];
+      if (nameObj.has(person)) {
+        sum += nameObj.get(person);
+      }
+    }
+    answer.push(sum);
+  }
   return answer;
 }
 
-solution('aukks', 'wbqd', 5);
+console.log(solution(name, yearning, photo));
